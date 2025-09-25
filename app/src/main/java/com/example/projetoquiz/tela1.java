7a74f012a3551d39bb14c844e31a983f57d15548
eduardosouzaramospedroni <edusouzapedroni@gmail.com>
2025-09-24 23:23:52 -0300

    tentei criar o clicklistener para mudar de tela ao clicar o botão continuar na tela1 mas não consegui

--- app/src/main/java/com/example/projetoquiz/tela1.java
+++ app/src/main/java/com/example/projetoquiz/tela1.java
@@ -1,6 +1,7 @@
 package com.example.projetoquiz;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +11,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class tela1 extends AppCompatActivity {
 
+    Button button_inicial;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela1);
+
+        Button botao = findViewById(R.id.button_inicial);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
